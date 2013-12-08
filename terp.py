1ecce6ef4365105c52c1facf1e10627f4c130b99
Darius Bacon <darius@wry.me>
2013-12-08 17:55:48 -0300

    more laziness; 4 factorial now works

--- terp.py
+++ terp.py
@@ -5,34 +5,65 @@ Interpreter for a toy dialect of Bicicleta.
 from collections import namedtuple
 from peglet import Parser, hug
 
-class VarRef(namedtuple('_VarRef', 'name')):
+class VarRef(object):
+    def __init__(self, name):
+        self.name = name
     def __repr__(self):
         return '@' + self.name
     def eval(self, env):
         return env[self.name]
 
-class NumberLiteral(namedtuple('_NumberLiteral', 'value')):
+class NumberLiteral(object):
+    def __init__(self, value):
+        self.value = value
     def __repr__(self):
         return str(self.value)
     def eval(self, env):
         return Number(self.value)
 
-class Call(namedtuple('_Call', 'receiver selector')):
+class Call(object):
+    def __init__(self, receiver, selector):
+        self.receiver = receiver
+        self.selector = selector
     def __repr__(self):
         return '%s.%s' % (self.receiver, self.selector)
     def eval(self, env):
         return call(self.receiver.eval(env), self.selector)
 
-class Extend(namedtuple('_Extend', 'base name bindings')):
+class Extend(object):
+    def __init__(self, base, name, bindings):
+        self.base = base
+        self.name = name
+        self.bindings = list(bindings)
     def __repr__(self):
         return '%s{%s: %s}' % (self.base, self.name,
                                ', '.join('%s=%s' % binding
                                          for binding in self.bindings))
     def eval(self, env):
-        return extend(self.base.eval(env), # XXX I'll bet this needs to be lazy
-                      {slot: lambda rcvr: expr.eval(extend_env(env,
-                                                               {self.name: rcvr}))
+        return Thunk(self, env)
+    def force(self, env):
+        return extend(self.base.eval(env),
+                      {slot: (lambda expr: lambda rcvr: expr.eval(extend_env(env,
+                                                                             {self.name: rcvr})))(expr)
                        for slot, expr in self.bindings})
+
+class Thunk(object): 
+    def __init__(self, expr, env):
+        assert isinstance(expr, Extend)
+        self.expr = expr
+        self.env = env
+    def __repr__(self):
+        return 'Thunk(%r)' % self.expr
+    def force(self):
+        return self.expr.force(self.env)
+    def get(self, key):
+        return self.force().get(key)
+    def __getitem__(self, key):
+        return self.force().__getitem__(key)
+    def items(self):
+        return self.force().items()
+    def __iter__(self):
+        return iter(self.force().items())
 
 def call(receiver, selector):
     return receiver[selector](receiver)
@@ -52,6 +83,7 @@ def Number(n):
                              Claim(n == call(operation, '_').get('__value__'))}}
 
 def Claim(value):
+    assert isinstance(value, bool)
     return true_claim if value else false_claim
 
 true_claim  = {'if': lambda _: {'()': lambda picking: call(picking, 'so')}}
@@ -62,8 +94,7 @@ def extend_env(env, bindings):
     result.update(bindings)
     return result
 
-initial_env = {'<>': {},
-               'top': {'a': lambda rcvr: 5}} # XXX remove
+initial_env = {'<>': {}}
 
 def attach_affixes(primary, *affixes):
     expr = primary
@@ -73,7 +104,8 @@ def attach_affixes(primary, *affixes):
 
 def attach(expr, affix): return affix[0](expr, *affix[1:])
 
-def mk_int(string): return NumberLiteral(int(string))
+def mk_int(wtf):
+    return NumberLiteral(int(wtf))
 
 def empty(): return VarRef('<>')
 def nameless(): return ''
@@ -115,18 +147,14 @@ _           = \s*
 ## parse('x = y', rule='bindings')
 #. (('x', @y),)
 
-## parse("{foo = {bar: x = y, '()'=x.a.b }}")
-#. (@<>{: foo=@<>{bar: x=@y, ()=@x.a.b}},)
+## wtf, = parse("{x=42, y=55}.x")
+## wtf
+#. @<>{: x=42, y=55}.x
+## wtf.eval(initial_env)['__value__']
+#. 42
 
-## bar, = parse("top.a")
-## bar.eval(initial_env)
-#. 5
-
-## foo, = parse("{_: b={blah: x=top}}.b.x.a")
-## foo
-#. @<>{_: b=@<>{blah: x=@top}}.b.x.a
-## foo.eval(initial_env)
-#. 5
+## parse("{y=42, x=55, z=137}.x")[0].eval(initial_env)['__value__']
+#. 55
 
 ## numlit, = parse("137")
 ## numlit
@@ -139,7 +167,7 @@ _           = \s*
 #. 138
 
 ## cmping, = parse("137.'=='(_=1).if(so=42, not=168)")
-## cmping == parse("137.'=='{_=1}.'()'.if{so=42, not=168}.'()'")[0]
+## repr(cmping) == repr(parse("137.'=='{_=1}.'()'.if{so=42, not=168}.'()'")[0])
 #. True
 ## cmping.eval(initial_env)['__value__']
 #. 168
@@ -149,7 +177,8 @@ fac, = parse("""
  fac = {fac:
         '()' = fac.n.'=='(_=0).if(so  = 1,
                                   not = fac.n.'*'(_ = env.fac(n = fac.n.'-'(_=1))))}
-}.fac(n=0)""")
+}.fac(n=4)""")
 ## fac
-#. @<>{env: fac=@<>{fac: ()=@fac.n.=={: _=0}.().if{: so=1, not=@fac.n.*{: _=@env.fac{: n=@fac.n.-{: _=1}.()}.()}.()}.()}}.fac{: n=0}.()
-# fac.eval(initial_env)
+#. @<>{env: fac=@<>{fac: ()=@fac.n.=={: _=0}.().if{: so=1, not=@fac.n.*{: _=@env.fac{: n=@fac.n.-{: _=1}.()}.()}.()}.()}}.fac{: n=4}.()
+## fac.eval(initial_env)['__value__']
+#. 24
